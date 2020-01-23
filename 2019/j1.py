74d8176b8791bf49ff5f03bf35b3650184888427
l0ckay <59676067+l0ckay@users.noreply.github.com>
2020-01-22 21:50:16 -0500

    Create j1.py

---
+++ 2019/j1.py
@@ -0,0 +1,21 @@
+# Variables for scores of Apples and Bananas
+apples = 0
+bananas = 0
+
+# Calculate score for apples
+for a in range(3, 0, -1): # a = 3, 2, 1 --> score multiplier
+    # Add the line input times the score multiplier to the score
+    apples += a * int(input())
+
+# Calculate score for bananas
+for b in range(3, 0, -1):
+    bananas += b * int(input())
+
+# Print winner
+if apples > bananas:
+    print("A")
+elif bananas > apples:
+    print("B")
+else:
+    print("T")
+    
